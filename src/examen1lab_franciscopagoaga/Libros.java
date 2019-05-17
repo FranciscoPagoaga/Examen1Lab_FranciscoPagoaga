6a4ba287a25d87d6ec8062f289b8da4cead2b58b
FranciscoPagoaga <50030247+FranciscoPagoaga@users.noreply.github.com>
2019-05-17 14:30:05 -0600

    Adelanto

---
+++ src/examen1lab_franciscopagoaga/Libros.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1lab_franciscopagoaga;
+
+/**
+ *
+ * @author Francisco Pagoaga
+ */
+public class Libros {
+    private String autor;
+    private String titulo;
+    private String genero;
+    private String editoriales;
+    private String idioma;
+    private int anioescrito;
+    private String isbn;
+    private int aniolibreria;
+    private String estado;
+    private String nombreprestado;
+    private int numcuenta;
+    
+}
